76bbb0f47db3b4f6b12793c9c18786e0aded260d
Bruno Gonçalves <brunovg@gmail.com>
2024-01-03 19:50:52 +0000

    refined logic on OtherTypeWithoutNamespace to extractChildAsOtherInObjectNode

--- xmltojsonlegacy/src/main/java/org/assimbly/xmltojsonlegacy/processor/xmltojson/elementnode/types/OtherTypeWithoutNamespace.java
+++ xmltojsonlegacy/src/main/java/org/assimbly/xmltojsonlegacy/processor/xmltojson/elementnode/types/OtherTypeWithoutNamespace.java
@@ -3,7 +3,10 @@ package org.assimbly.xmltojsonlegacy.processor.xmltojson.elementnode.types;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.lang3.StringUtils;
+import org.assimbly.xmltojsonlegacy.Constants;
 import org.assimbly.xmltojsonlegacy.processor.xmltojson.elementnode.ElementNodeTransaction;
+import org.assimbly.xmltojsonlegacy.utils.ElementChecker;
 import org.assimbly.xmltojsonlegacy.utils.ElementUtils;
 import org.assimbly.xmltojsonlegacy.utils.ExtractUtils;
 import org.w3c.dom.Element;
@@ -20,8 +23,11 @@ public class OtherTypeWithoutNamespace implements ElementNodeTransaction {
     ) {
         if(!typeHints) {
             // extract child as other type and add into the array node
+            int children = ElementUtils.calculateNumberOfChildren(childElement);
+            boolean classAttrOnChildElementIsNUll = ElementChecker.isElementAttributeNull(childElement, Constants.JSON_XML_ATTR_CLASS);
             if((level == 0 && numberOfChildren > 1) ||
-                    (numberOfChildren == 1 && ElementUtils.calculateNumberOfChildren(childElement) > 1)
+                    (numberOfChildren == 1 && children >= 1 && !classAttrOnChildElementIsNUll) ||
+                    !ElementUtils.areSiblingsNamesEqual(childElement)
             ) {
                 ExtractUtils.extractChildAsOtherInObjectNode(
                         level, rootObjectNode, numberOfSiblings, parentClass, classAttr, (Element) childNode, childElement,
