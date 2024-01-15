3303ff7998a5b24218292e2eed6fe34fcc0500a7
Bruno Gonçalves <brunovg@gmail.com>
2024-01-15 15:39:32 +0000

    new isJsonNodeInOneLevelAndWithNamespace method, that checks if jsonNode have only one level and have declared namespaces

--- xmltojsonlegacy/src/main/java/org/assimbly/xmltojsonlegacy/utils/JsonUtils.java
+++ xmltojsonlegacy/src/main/java/org/assimbly/xmltojsonlegacy/utils/JsonUtils.java
@@ -1,12 +1,17 @@
 package org.assimbly.xmltojsonlegacy.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 import org.apache.commons.lang3.StringUtils;
+import org.assimbly.xmltojsonlegacy.Constants;
 
+import javax.xml.XMLConstants;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class JsonUtils {
 
+    // get and set namespace from/to HashMap xmlnsMap
     public static String getAndSetNamespace(HashMap<String, String> xmlnsMap, String key, String value) {
         if(xmlnsMap.containsKey(key)) {
             return null;
@@ -15,6 +20,7 @@ public class JsonUtils {
         return value;
     }
 
+    // get root tag name
     public static String getRootTagName(JsonNodeType nodeType, String rootName, String arrayName) {
         boolean isRootNameSpecified = !StringUtils.isEmpty(rootName);
         boolean isArrayNameSpecified = !StringUtils.isEmpty(arrayName);
@@ -28,6 +34,25 @@ public class JsonUtils {
             }
         }
         return null;
+    }
+
+    // checks if jsonNode have only one level and have declared namespaces
+    public static boolean isJsonNodeInOneLevelAndWithNamespace(JsonNode jsonNode) {
+        boolean isNamespacePresent = false;
+        if (jsonNode.isObject()) {
+            Iterator<String> fieldNames = jsonNode.fieldNames();
+            while (fieldNames.hasNext()) {
+                String fieldName = fieldNames.next();
+                JsonNode fieldNode = jsonNode.get(fieldName);
+                if (fieldNode.isObject() || fieldNode.isArray()) {
+                    return false;
+                }
+                if(fieldName.startsWith(Constants.JSON_XML_ATTR_PREFIX + XMLConstants.XMLNS_ATTRIBUTE)){
+                    isNamespacePresent = true;
+                }
+            }
+        }
+        return isNamespacePresent;
     }
 
 }
