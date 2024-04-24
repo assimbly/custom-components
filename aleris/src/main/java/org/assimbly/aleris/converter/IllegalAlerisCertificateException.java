5c0338f1d8d7ec1f38e1bd879bf8f2b3a2ca52ad
Raymond Meester <raymondmeester@gmail.com>
2024-04-24 16:28:33 +0200

    removed archived components

--- aleris/src/main/java/org/assimbly/aleris/converter/IllegalAlerisCertificateException.java
+++
@@ -1,7 +0,0 @@
-package org.assimbly.aleris.converter;
-
-public class IllegalAlerisCertificateException extends RuntimeException {
-    public IllegalAlerisCertificateException(String message) {
-        super(message);
-    }
-}
