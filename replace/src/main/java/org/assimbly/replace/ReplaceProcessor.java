8d311666b07f2164714d79390f053a8e5695b2ae
Raymond Meester <raymondmeester@gmail.com>
2024-06-04 16:32:36 +0200

    add match by group in replace component

--- replace/src/main/java/org/assimbly/replace/ReplaceProcessor.java
+++ replace/src/main/java/org/assimbly/replace/ReplaceProcessor.java
@@ -34,10 +34,27 @@ public class ReplaceProcessor implements Processor {
             replaceWith = ExchangeHelper.interpolate(replaceWith, exchange);
 
         Pattern pattern = Pattern.compile(regex, config.getFlagsMagicConstant());
-        String result = pattern.matcher(body).replaceAll(replaceWith);
+
+        String result = "";
+        if(config.getGroup() > 0){
+            result = replaceGroup(regex,body,config.getGroup(),replaceWith);
+        }else{
+            result = pattern.matcher(body).replaceAll(replaceWith);
+        }
 
         exchange.getIn().setBody(result);
 
+    }
+
+    public static String replaceGroup(String regex, String source, int groupToReplace, String replacement) {
+        return replaceGroup(regex, source, groupToReplace, 1, replacement);
+    }
+
+    public static String replaceGroup(String regex, String source, int groupToReplace, int groupOccurrence, String replacement) {
+        Matcher m = Pattern.compile(regex).matcher(source);
+        for (int i = 0; i < groupOccurrence; i++)
+            if (!m.find()) return source; // pattern not met, may also throw an exception here
+        return new StringBuilder(source).replace(m.start(groupToReplace), m.end(groupToReplace), replacement).toString();
     }
 
 }
