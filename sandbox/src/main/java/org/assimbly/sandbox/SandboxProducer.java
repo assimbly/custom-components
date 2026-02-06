6dbd6f9d4f112248703fd867b8852c4f1a47dc37
Bruno Gonçalves <brunovg@gmail.com>
2026-02-06 18:48:02 +0000

    isolate and secure Groovy script execution

--- sandbox/src/main/java/org/assimbly/sandbox/SandboxProducer.java
+++ sandbox/src/main/java/org/assimbly/sandbox/SandboxProducer.java
@@ -1,9 +1,9 @@
 package org.assimbly.sandbox;
 
 import org.apache.camel.Exchange;
-import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.component.language.LanguageEndpoint;
 import org.apache.camel.component.language.LanguageProducer;
+import org.assimbly.sandbox.executors.GroovySandboxExecutor;
 
 import java.util.UUID;
 
@@ -16,38 +16,28 @@ public class SandboxProducer extends LanguageProducer {
         super(endpoint);
     }
 
-    /**
-     * Overrides the process method to enable and disable the custom SecurityManager.
-     */
     @Override
     public void process(Exchange exchange) throws Exception {
+        String language = getEndpoint().getLanguage().toString();
+
+        if (language.toLowerCase().contains("groovy")) {
+            String script = getEndpoint().getScript();
+            if (script == null) {
+                script = exchange.getIn().getBody(String.class);
+            }
+            GroovySandboxExecutor.execute(script, exchange);
+        } else {
+            executeWithSecurity(exchange);
+        }
+    }
+
+    private void executeWithSecurity(Exchange exchange) throws Exception {
         UUID uuid = UUID.randomUUID();
-
         SandboxSecurityManager.getInstance().secure(uuid);
-
-        /*
-         * Comment out setting the security manager as it causes the whole JVM to be in a Sandbox
-         * and since we run asynchronous flows we do not want that.
-         *
-         * if(System.getSecurityManager() == null)
-         *   System.setSecurityManager(SandboxSecurityManager.getInstance());
-         *
-         */
-
-        boolean failed = false;
-
         try {
             super.process(exchange);
-        } catch (SecurityException e) {
-            failed = true;
         } finally {
             SandboxSecurityManager.getInstance().unsecure(uuid);
-
-            System.setSecurityManager(null);
-
-            if (failed) {
-                throw new RuntimeCamelException("Security breach");
-            }
         }
     }
 }
